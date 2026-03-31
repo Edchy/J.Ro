02115311e99e82311dd206f112850b45cbda240a
edch <soderblomed@gmail.com>
2026-03-31 12:56:25 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/J.Ro" : "/",
+});
